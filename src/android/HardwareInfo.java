595a1ec11c27bfa9783e847d82d27c794d8d549c
Antony <arantony@augustasoftsol.com>
2018-09-25 20:43:33 +0530

    get memory details

--- src/android/HardwareInfo.java
+++ src/android/HardwareInfo.java
@@ -14,11 +14,13 @@ import android.os.Environment;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.MemoryInfo;
+import java.io.BufferedReader;
 import java.io.File;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import android.os.Build;
+import java.io.IOException;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -91,22 +93,17 @@ public class HardwareInfo extends CordovaPlugin {
             byteArry = new byte[1024];
             try{
                 processBuilder = new ProcessBuilder(DATA);
-
                 process = processBuilder.start();
-
                 inputStream = process.getInputStream();
-
                 while (inputStream.read(byteArry) != -1) {
-
                     Holder = Holder + new String(byteArry);
                 }
-
                 inputStream.close();
-
             }
             catch (IOException ex) {
                 callbackContext.error("Error . "+ ex);
             }
+            Holder = getInfo();
             callbackContext.success(Holder);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
@@ -175,5 +172,25 @@ public class HardwareInfo extends CordovaPlugin {
         } else {
             return 0;
         }
+    }
+    
+    private String getInfo() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("abi: ").append(Build.CPU_ABI).append("\n");
+        if (new File("/proc/cpuinfo").exists()) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(new File("/proc/cpuinfo")));
+                String aLine;
+                while ((aLine = br.readLine()) != null) {
+                    sb.append(aLine + "\n");
+                }
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 }
