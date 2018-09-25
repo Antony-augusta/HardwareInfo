c244027aa1817df0d9ff260862dee1173405f72a
Antony <arantony@augustasoftsol.com>
2018-09-25 10:29:04 +0530

    first commit

---
+++ src/ios/HardwareInfo.m
@@ -0,0 +1,28 @@
+/********* HardwareInfo.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface HardwareInfo : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation HardwareInfo
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
