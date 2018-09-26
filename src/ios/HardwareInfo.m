5a1f772b4a504dba3ceb055713d27f814a3026e2
Antony-augusta <33082343+Antony-augusta@users.noreply.github.com>
2018-09-26 16:01:39 +0530

    ios ram and disk space

--- src/ios/HardwareInfo.m
+++ src/ios/HardwareInfo.m
@@ -1,6 +1,8 @@
 /********* HardwareInfo.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
+#import <mach/mach.h>
+#import <mach/mach_host.h>
 
 @interface HardwareInfo : CDVPlugin {
   // Member variables go here.
@@ -46,12 +48,44 @@
 - (void)RAMInfo:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-  long physicalMemoryvalue = [[NSProcessInfo processInfo] physicalMemory];
+    uint64_t totalSpace = 0;
+    uint64_t totalFreeSpace = 0;
+    long physicalMemoryvalue = [[NSProcessInfo processInfo] physicalMemory];
     NSString *rammemory = [NSString stringWithFormat:@"%ld", physicalMemoryvalue];
-
+    __autoreleasing NSError *error = nil;
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSDictionary *dictionary = [[NSFileManager defaultManager] attributesOfFileSystemForPath:[paths lastObject] error: &error];
     
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:rammemory];
+    NSString *totalstorage;
+    NSString *totalfreestorage;
+    
+    if (dictionary) {
+        NSNumber *fileSystemSizeInBytes = [dictionary objectForKey: NSFileSystemSize];
+        NSNumber *freeFileSystemSizeInBytes = [dictionary objectForKey:NSFileSystemFreeSize];
+        totalSpace = [fileSystemSizeInBytes unsignedLongLongValue];
+        totalFreeSpace = [freeFileSystemSizeInBytes unsignedLongLongValue];
+        totalstorage = [NSString stringWithFormat:@"%llu", ((totalSpace/1024ll)/1024ll)];
+        totalfreestorage = [NSString stringWithFormat:@"%llu", ((totalFreeSpace/1024ll)/1024ll)];
+        
+    }
+    else {
+        NSLog(@"Error Obtaining System Memory Info: Domain = %@, Code = %ld", [error domain], (long)[error code]);
+    }
+    
+    
+    NSDictionary *firstJsonDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                         totalfreestorage, @"totalfreestorage",
+                                         totalstorage, @"totalstorage",
+                                         rammemory, @"ramtotal",
+                                         nil];
+    
+    NSMutableArray * arr = [[NSMutableArray alloc] init];
+    
+    [arr addObject:firstJsonDictionary];
+   
+    NSData *jsonData2 = [NSJSONSerialization dataWithJSONObject:arr options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData2 encoding:NSUTF8StringEncoding];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:jsonString];
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
